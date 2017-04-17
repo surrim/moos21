ba3a8543d620472b978be5dbeb352fb32b600185
surrim <surrim@happyhydro.org>
2017-04-17 17:31:58 +0200

    removed ugly src/moos21_private.h

--- src/helpmenu.cpp
+++ src/helpmenu.cpp
@@ -1,7 +1,6 @@
 #include "base.h"
 
 #include "infodialog.h"
-#include "moos21_private.h"
 #include <wx/fileconf.h>
 
 void MainFrame::OnOnlineReadme(wxCommandEvent& WXUNUSED(event)) {
@@ -14,6 +13,10 @@ void MainFrame::OnMailBugs(wxCommandEvent& WXUNUSED(event)) {
 
 void MainFrame::OnAbout(wxCommandEvent& WXUNUSED(event)) {
 	InfoDialog(this, langIni, font.GetChosenFont(), langIni->Read(wxT("translations/menus/help/about"), wxT("About Moos 2.1...")),
-			   wxT("MoonNet Omnipresent Observation System ")+wxString(_(VER_STRING))
-			   +wxT("\nmoos2.1 is an Inside Earth Operations application (www.insideearth.de)\nwritten by mensi (mensi@insideearth.de), modified by surrim\n\nThis program is freeware WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.")).ShowModal();
+			   wxT(R"raw(
+MoonNet Omnipresent Observation System 2.1.1
+moos2.1 is an Inside Earth Operations application (www.insideearth.de) written by mensi (mensi@insideearth.de), modified by surrim.
+
+This program is freeware WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+			   )raw")).ShowModal();
 }
